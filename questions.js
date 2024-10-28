69f098481fe3e57b10383b8084ecc8700d4c5bf9
SohamJoshi0510 <sohamjoshi20032002@gmail.com>
2024-10-28 08:10:57 +0530

    Add questions

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+        {   
+            numb: 1,
+            question: "What is the capital of France?",
+            answer: "A. Paris",
+            options: [
+                "A. Paris",
+                "B. London",
+                "C. Berlin",
+                "D. Madrid",
+            ]
+        },
+        {   
+            numb: 2,
+            question: "Who is the author of the Harry Potter series?",
+            answer: "B. J.K. Rowling",
+            options: [
+                "A. J.R.R. Tolkien",
+                "B. J.K. Rowling",
+                "C. Stephen King",
+                "D. George R.R. Martin",
+            ]
+        },
+        {   
+            numb: 3,
+            question: "What is the chemical symbol for the element Gold?",
+            answer: "D. Au",
+            options: [
+                "A. Go",
+                "B. Gd",
+                "C. Gl",
+                "D. Au",
+            ]
+        },
+        {   
+            numb: 4,
+            question: "Which planet is known as the Red Planet?",
+            answer: "C. Mars",
+            options: [
+                "A. Jupiter",
+                "B. Saturn",
+                "C. Mars",
+                "D. Venus",
+            ]
+        },
+        {   
+            numb: 5,
+            question: "What is the powerhouse of the cell?",
+            answer: "B. Mitochondria",
+            options: [
+                "A. Nucleus",
+                "B. Mitochondria",
+                "C. Ribosome",
+                "D. Endoplasmic Reticulum",
+            ]
+        }
+]
+
+
