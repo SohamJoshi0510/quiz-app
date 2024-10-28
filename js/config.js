63c46a0cad053388eb653b2d570f04a411e763c8
SohamJoshi0510 <sohamjoshi20032002@gmail.com>
2024-10-28 08:06:50 +0530

    update /js/config

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_jkXIgkEFw', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '47kp0h42o880qqr8k9mlcedg6e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
